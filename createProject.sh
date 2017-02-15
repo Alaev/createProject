2906bfcaf27ec7f990f69ba2497087dcd7dc21b3
Michael Alaev <Alaev@users.noreply.github.com>
2017-02-15 22:21:16 +0200

    Create createProject.sh

---
+++ createProject.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir build;
+mkdir build/css/;
+mkdir build/js/;
+touch build/js/app.js;
+mkdir build/stylus/;
+touch build/stylus/main.styl;
+mkdir build/vendor/;
+touch build/index.html;
+
+touch gulpfile.js;
+touch package.json;
+
+rm createProject.sh
